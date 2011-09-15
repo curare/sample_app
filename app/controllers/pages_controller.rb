ec2d6e1d701d10daeaf578650a2e423d3ea9dcf0
stasomatic <stas@newdel.net>
2011-09-15 16:41:39 -0400

    Revoved default index.html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,17 @@
 class PagesController < ApplicationController
   def home
-    @base_title = "Ruby on Rails Tutorial Sample App |"
     @title = "Home"
   end
 
   def contact
-    @base_title = "Ruby on Rails Tutorial Sample App |"
     @title = "Contact"
   end
   
   def about
-    @base_title = "Ruby on Rails Tutorial Sample App |"
     @title = "About"
   end
   
   def help
-    @base_title = "Ruby on Rails Tutorial Sample App |"
     @title = "Help"
   end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,9 +4,9 @@ describe PagesController do
   render_views
   
   before(:each) do
-    @base_title = "Ruby on Rails Tutorial Sample App"
+      @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  
+      
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -15,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                          :content => @base_title + " | Home")
+                          :content => "#{@base_title} | Home")
      end
   end
 
@@ -27,7 +27,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                                    :content => @base_title + " | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
   end
   
@@ -39,7 +39,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                                    :content => @base_title + " | About")
+                                    :content => "#{@base_title} | About")
     end
   end
   
@@ -51,7 +51,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                                    :content => @base_title + " | Help")
+                                    :content => "#{@base_title} | Help")
     end
   end
 end
