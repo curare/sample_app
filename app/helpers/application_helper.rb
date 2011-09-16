36a37cbb8660048009880ebe661a87c5888bc95c
stasomatic <stas@newdel.net>
2011-09-16 11:14:25 -0400

    Finished Chapter 5 excersizes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo(the_logo = "custom_logo.png",alt = "Stas's Sample App",css_class = "thick_border")
+    the_logo = @the_logo unless @the_logo.nil?
+    alt = @alt unless @alt.nil?
+    css_class = @css_class unless @css_class.nil?
+    
+    image_tag(the_logo, :alt => alt, :class => css_class)
+  end
+  	
 end
